a3db6b17e8f55c5d5d2a492da8c6d66004b463b2
Ieunn <898354552@qq.com>
2024-09-19 18:03:52 +0800

    Import pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './index.css'
 import Vue3TouchEvents from "vue3-touch-events";
 
-createApp(App)
-  .use(router)
-  .use(Vue3TouchEvents)
-  .mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+   .use(router)
+   .use(Vue3TouchEvents)
+   .mount('#app')
